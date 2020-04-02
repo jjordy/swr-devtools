6c3f606e2b84d6086b0ec6f5ba8ef7c7d66da43f
Jordan Addison <jordanrileyaddison@gmail.com>
2020-04-01 22:41:29 -0500

    fixes

--- src/SWRDevtools/Panel/usePanelState.tsx
+++ src/SWRDevtools/Panel/usePanelState.tsx
@@ -4,8 +4,7 @@ import { UsePanelStateProps } from "../types";
 
 export default function usePanelState({
   toolbarPosition,
-  show,
-  previousToolbarPosition
+  show
 }: UsePanelStateProps) {
   const [theme, setTheme] = useState("Dark");
   const [isDragging, setIsDragging] = useState(false);
