fd483b7197a50e97b5475e030f483f9fe81cb326
Jordan Addison <jordanrileyaddison@gmail.com>
2021-04-26 14:54:06 -0500

    devtools changes.

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,24 +4,5 @@ module.exports = {
   features: {
     postcss: false
   },
-  stories: ["./**/*.stories.@(tsx|jsx)"],
-  // webpackFinal: async (config) => {
-  //   config.module.rules.push({
-  //     test: /\.(ts|tsx)$/,
-  //     use: [
-  //       {
-  //         loader: require.resolve("ts-loader"),
-  //       },
-  //       // Optional
-  //       {
-  //         loader: require.resolve("react-docgen-typescript-loader"),
-  //         // options: {
-  //         //   tsconfigPath: path.resolve(__dirname, "../tsconfig.json")
-  //         // }
-  //       },
-  //     ],
-  //   });
-  //   config.resolve.extensions.push(".ts", ".tsx");
-  //   return config;
-  // },
+  stories: ["./**/*.stories.@(tsx|jsx)"]
 };
